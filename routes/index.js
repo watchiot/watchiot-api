5269ae00c7640c4f1495215b0b58c61789518b87
gorums <acksecurity@hotmail.com>
2016-06-25 02:09:08 -0500

    Integration with postgres

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'WatchIoT API' });
+});
+
+router.post('/', function(req, res, next) {
+  var auth = req.header("Authorization")
+
+  res.json({'auth': auth})
 });
 
 module.exports = router;
