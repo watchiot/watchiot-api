203c0c83e6743a7e3e96ae16b718c6460f3c810b
gorums <acksecurity@hotmail.com>
2016-07-13 00:28:53 -0500

    Add more middlewares

---
+++ middlewares/auth/index.js
@@ -0,0 +1,50 @@
+var models = require('../../models/index');
+
+function findUser(arrUserAndApikey, fUserAuth) {
+
+     // find user with username = arrUserAndApikey[0] and api_key = arrUserAndApikey[1]
+     // and with a primary email
+    models.users.findOne({
+            include: [
+                {
+                    model: models.api_keys,
+                    where: {api_key: arrUserAndApikey[1]}
+                },
+                {
+                    model: models.emails,
+                    where: {primary: true}
+                }],
+            where: {username: arrUserAndApikey[0]}
+        })
+        .then(function (user) {
+            fUserAuth(user);
+        });
+};
+
+function authSplit(authHeader) {
+    // format: {USERNAME} {API_KEY}
+    return authHeader.split(" ");
+}
+
+module.exports = {
+    auth: function (req, res, next) {
+        var arrUserAndApikey = authSplit(req.header("Authorization"));
+
+        if (arrUserAndApikey.length !== 2) {
+            res.status(400).send('BAD REQUEST');
+            return;
+        }
+
+        findUser(arrUserAndApikey, function (user) {
+                req.user = user;
+                next();
+            });
+    },
+    isAuth: function (req, res, next) {
+        if(req.user){
+            next();
+        } else {
+            res.status(401).send('UNAUTHORIZED');
+        }
+    }
+}
