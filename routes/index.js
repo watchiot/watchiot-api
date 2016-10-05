1034239ad74022c76cc046ae4383f3c6e735caca
gorums <acksecurity@hotmail.com>
2016-10-04 22:40:49 -0400

    Add my first mongoose model.

--- routes/index.js
+++ routes/index.js
@@ -19,9 +19,8 @@ router.post('/', function (req, res) {
 
 router.post('/:space/:project',
     project.project, project.isStatus, project.isReady, project.validIp,
-    project.reqPerhour, project.limit, project.hasMetric,
-    project.validMetrics, project.evalMetrics,
-    project.saveMetrics, project.notif,
+    project.reqPerhour, project.limit, project.hasMetric, project.validMetrics,
+    project.evalMetrics, project.saveMetrics, project.notif,
     function (req, res) {
         res.json(JSON.stringify(new Response(200, 'all is fine', {})));
     });
